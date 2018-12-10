e4b03e462d70b6b82eed829aca15038b4593086b
rliu666 <rliu666@sina.cn>
2018-12-10 14:51:44 +0800

    图片管理手动录入优化

--- userservice/src/main/java/com/syfri/userservice/model/PictureVO.java
+++ userservice/src/main/java/com/syfri/userservice/model/PictureVO.java
@@ -25,6 +25,10 @@ public class PictureVO extends ValueObject implements Serializable{
 	private String alterName;	//修改人
 	private String alterTime;	//修改时间
 	private String imgStr;	//列表图片
+	private String reserve1;	//
+	private String reserve2;	//
+	private String reserve3;	//
+	private String reserve4;
 
 	public String getImgStr() {
 		return imgStr;
@@ -133,5 +137,37 @@ public class PictureVO extends ValueObject implements Serializable{
 	}
 	public void setAlterTime(String alterTime){
 		this.alterTime = alterTime;
+	}
+
+	public String getReserve1() {
+		return reserve1;
+	}
+
+	public void setReserve1(String reserve1) {
+		this.reserve1 = reserve1;
+	}
+
+	public String getReserve2() {
+		return reserve2;
+	}
+
+	public void setReserve2(String reserve2) {
+		this.reserve2 = reserve2;
+	}
+
+	public String getReserve3() {
+		return reserve3;
+	}
+
+	public void setReserve3(String reserve3) {
+		this.reserve3 = reserve3;
+	}
+
+	public String getReserve4() {
+		return reserve4;
+	}
+
+	public void setReserve4(String reserve4) {
+		this.reserve4 = reserve4;
 	}
 }
